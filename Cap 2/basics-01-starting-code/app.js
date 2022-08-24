f69549c53b2676a436f9b6aa4955d57bcfeb0034
Josue Acuña <josueacuna.047@gmail.com>
2022-08-23 23:24:10 -0500

    Trabajando con metodos  vue

--- Cap 2/basics-01-starting-code/app.js
+++ Cap 2/basics-01-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finalizamos el curso d Vue ',
             vueLink: 'https://vuejs.org/'
         }
+    },
+    methods: {
+        outputGoal(){
+            const RandomNumber = Math.random();
+            if (RandomNumber < 0.5){
+                return 'aprende vue';
+            }else{
+                return 'Vue aprendido'
+            }
+        }
     }
 });
 

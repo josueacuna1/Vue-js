fa253e567a433c19a78253b1cd6eb139381a7e88
Josue Acuña <josueacuna.047@gmail.com>
2022-08-23 23:31:49 -0500

    Llamando data en un metodo

--- Cap 2/basics-01-starting-code/app.js
+++ Cap 2/basics-01-starting-code/app.js
@@ -1,17 +1,18 @@
 const app = Vue.createApp({
     data(){
         return{
-            courseGoal: 'Finalizamos el curso d Vue ',
+            courseGoalA: 'Empezamos el curso d Vue ',
+            courseGoalB: 'Finalizamos el curso d Vue ',
             vueLink: 'https://vuejs.org/'
-        }
+        };
     },
     methods: {
         outputGoal(){
             const RandomNumber = Math.random();
             if (RandomNumber < 0.5){
-                return 'aprende vue';
+                return this.courseGoalA;
             }else{
-                return 'Vue aprendido'
+                return this.courseGoalB;
             }
         }
     }
